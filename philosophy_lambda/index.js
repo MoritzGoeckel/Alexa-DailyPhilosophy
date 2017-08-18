9b1def2fae2cc0ef02bc49494dced87c769661a4
moritzgoeckel <moritz.goeckel@gmail.com>
2017-08-18 21:16:07 +0200

    Send out for certification

---
+++ philosophy_lambda/index.js
@@ -0,0 +1,46 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+
+'use strict';
+
+const quotes = require('./quotes.json');
+
+const Alexa = require('alexa-sdk');
+
+const APP_ID = "amzn1.ask.skill.d76a0a14-b806-4753-a3fd-6142a1b06ba3";
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('getquote')
+    },
+    'getquote': function () {
+        // Get a random space fact from the space facts list
+        // Use this.t() to get corresponding language data
+        
+        //const factArr = this.t('FACTS');
+
+        //JSON.parse(process.env.resources)[2]
+
+        let rand = Math.random();
+        let max = quotes.length - 1;
+        //rand + " " + max + " " + 
+        this.emit(':tellWithCard', quotes[parseInt(rand * max)].replace(".", ". "));
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', "This is the help part");
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    //alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
